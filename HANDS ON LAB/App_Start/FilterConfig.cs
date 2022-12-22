0c6e7caefc8d85651451e48db58e57bdc253d23c
Vajja Anuja <Vajja Anuja@anujavm>
2022-12-22 05:10:12 +0000

    Add project files.

---
+++ HANDS ON LAB/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace HANDS_ON_LAB
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ HANDS ON LAB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HANDS_ON_LAB.MvcApplication" Language="C#" %>
